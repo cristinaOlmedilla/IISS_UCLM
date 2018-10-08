6965e94dc2747ba401ea92f78b5fe570d4751ce4
olme <olme@littleCrispis>
2018-10-08 18:11:30 +0200

    MazeState almost finished

--- IISS_Assignment1/src/problems/maze/MazeState.java
+++ IISS_Assignment1/src/problems/maze/MazeState.java
@@ -1,5 +1,7 @@
 package problems.maze;
 
+import java.util.LinkedList;
+
 import problems.maze.MazeState;
 import search.State;
 import utils.Position;
@@ -11,22 +13,66 @@ public class MazeState extends State implements Cloneable{
 	
 	/** An state is includes a position given by the coordinates (x,y) */
 	public Position position;
+	public int cheeseCounter;
+	public int damage;
+	
+	public LinkedList<Position> eatenCheeses;
+	
+	//InitialState MazeState
+	public MazeState(int x, int y){
+		this.position = new Position(x,y);
+		this.eatenCheeses = new LinkedList<>(); 
+		this.cheeseCounter = eatenCheeses.size();		
+	}
+	public MazeState(int[] initialP){
+		this.position = new Position(initialP);
+		this.eatenCheeses = new LinkedList<>(); 
+		this.cheeseCounter = eatenCheeses.size();		
+	}
+	
+	//Overwrite States
+	public MazeState(int x, int y, LinkedList<Position>eCheeses, int dam){
+		this.position = new Position(x,y);
+		this.eatenCheeses = eCheeses; 
+		this.cheeseCounter = eatenCheeses.size();
+		this.damage = dam;
+	}
+	public MazeState(int[] regularP, LinkedList<Position>eCheeses, int dam){
+		this.position = new Position(regularP);
+		this.eatenCheeses = eCheeses; 
+		this.cheeseCounter = eatenCheeses.size();
+		this.damage = dam;
+	}
 
 	@Override
 	public boolean equals(Object anotherState) {
 		// TODO Auto-generated method stub
-		return false;
+		MazeState aState = (MazeState)anotherState;
+		if(this.position.equals(aState.position)
+				&&this.eatenCheeses.equals(aState.eatenCheeses)
+				&&this.damage==aState.damage){
+			return true;
+		}else{
+			return false;
+				}
+	
 	}
 
 	@Override
 	public int hashCode() {
 		// TODO Auto-generated method stub
+		//NO CHABEMOS!!!
+		//return Objects.hash(field1, filed2.... );
 		return 0;
+		
 	}
 
 	@Override
 	public String toString() {
 		// TODO Auto-generated method stub
-		return null;
+		return ("State: Position-> "+this.position.toString()
+				+" Eaten Cheeses-> "+this.cheeseCounter
+				+" Damage-> "+this.damage);
+		
 	}
 }
